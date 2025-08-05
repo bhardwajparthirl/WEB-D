7226867e7f8e266a5abb504ff42aa92c4a23dd6a
bhardwajparthirl <parthirlplays@gmail.com>
2025-08-05 08:19:52 +0000

    math and num

--- string/numbers/nums_and_maths.js
+++ string/numbers/nums_and_maths.js
@@ -12,3 +12,17 @@ console.log(otherNumber.toPrecision(4));
 
 const hundreds =1000000
 console.log(hundreds.toLocaleString("en-IN"));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,6,2,1));
+console.log(Math.max(4,6,2,1));
+
+console.log(Math.random());  //random keyword create any value bte 0 to 1
+
+console.log(Math.random()*10+1);
+console.log(Math.random(Math.random()*10)+1);
+
